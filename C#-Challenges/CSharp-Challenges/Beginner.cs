962007fe6889005e74be13dea07b2551249e850c
YuriJesusVedovate <Yurivedovate@hotmail.com>
2024-08-31 12:32:33 -0300

    Update Beginner.cs

--- C#-Challenges/CSharp-Challenges/Beginner.cs
+++ C#-Challenges/CSharp-Challenges/Beginner.cs
@@ -143,4 +143,15 @@ public class Beginner
     }
     
     public void Beginner1015DistanceBetweenTwoPoints()
+    {
+        string[] p1 = Console.ReadLine()?.Split(' ') ?? Array.Empty<string>();
+        string[] p2 = Console.ReadLine()?.Split(' ') ?? Array.Empty<string>();
+        double x1 = double.Parse(p1[0]);
+        double y1 = double.Parse(p1[1]);
+        double x2 = double.Parse(p2[0]);
+        double y2 = double.Parse(p2[1]);
+        double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        Console.WriteLine(distance.ToString("F4"));
+    }
+    
 }
